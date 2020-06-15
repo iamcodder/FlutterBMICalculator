402f46657bf3b45e36f8d4265ef8b102e50b4ee9
Süleyman SEZER <me.iamcodder@gmail.com>
2020-06-15 21:54:00 +0300

    Calculate home eklendi
    Yeniden kullanılabilir widget eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,16 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF090C22),
+        scaffoldBackgroundColor: Color(0xFF090C22),
+        appBarTheme: AppBarTheme(
+          elevation: 16.0,
+          centerTitle: true,
+          brightness: Brightness.dark,
+        ),
+      ),
       home: InputPage(),
     );
   }
@@ -16,6 +26,10 @@ class InputPage extends StatefulWidget {
   _InputPageState createState() => _InputPageState();
 }
 
+const bottomBarHeight = 80.0;
+const cardBackgroundColor = Color(0xFF1D1F33);
+const bottomBarColor = Color(0xFFEA1556);
+
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -23,11 +37,60 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                CustomCard(bdColor: cardBackgroundColor),
+                CustomCard(bdColor: cardBackgroundColor),
+              ],
+            ),
+          ),
+          CustomCard(bdColor: cardBackgroundColor),
+          Expanded(
+            child: Row(
+              children: [
+                CustomCard(bdColor: cardBackgroundColor),
+                CustomCard(bdColor: cardBackgroundColor),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomBarColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomBarHeight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'CALCULATE YOUR BMI',
+                  style: TextStyle(fontSize: 18.0, fontFamily: 'BalsamiqSans'),
+                )
+              ],
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final Color bdColor;
+
+  const CustomCard({@required this.bdColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: bdColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
